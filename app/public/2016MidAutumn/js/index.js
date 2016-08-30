f96d49672c59b3e6b178b5c911b2ca3a46f86bab
neo-home-win7 <yyjpig@aliyun.com>
2016-08-31 01:06:04 +0800

    5

--- app/public/2016MidAutumn/js/index.js
+++ app/public/2016MidAutumn/js/index.js
@@ -38,21 +38,82 @@ $(document).ready(function () {
     $('#myTemplate').remove();
 
 
-    $('.module-guide').on('click', function () {
-        console.log('click');
+    $('.module-guide').on('swipe', function () {
+        console.log('swipe');
         $(this).remove();
         $('.module-prologue').addClass('show');
+        setTimeout(function () {
+            $('.module-prologue').removeClass('show');
+            $('.module-beauty').addClass('show');
+        }, 5 * 1000);
+    });
+
+
+    $('.module-beauty .btn-help').on('tap', function () {
+        $('.module-beauty').removeClass('show');
+        $('.module-cakes').addClass('show');
+        myScroll && myScroll.refresh();
+    });
+
+
+
+    $('.module-cakes .btn-select').on('tap', function () {
+        var pageX = myScroll.currentPage.pageX + 1;
+        $('.module-cakes').removeClass('show');
+
+        $('.module-detail .wrapper-result img').attr('src', './img/result-' + rand(1, 11) + '.png');
+        $('.module-detail .circle img').attr('src', './img/circle-' + pageX + '.png');
+        $('.module-detail .ticket-text img').attr('src', './img/ticket-text-' + pageX + '.png');
+        $('.module-detail .cake-detail').hide().filter('.cake-detail-' + pageX).show();
+
+        $('.module-detail').addClass('show');
+    });
+
+
+    $('.module-detail .btn-share').on('tap', function () {
+        $('.module-share-guide').css({
+            width: '100%',
+            height: $(document).height()
+        }).show();
+    });
+    $('.module-detail .wrapper-again').on('tap', function () {
+        $('.module-detail').removeClass('show');
+        $('.module-cakes').addClass('show');
     });
 
 
     /**
      * iscroll
      */
-    var myScroll = new window.IScroll('#wrapper-scroller', {
+    var myScroll = window.myScroll = new window.IScroll('#wrapper-scroller', {
         scrollX: true,
         scrollY: false,
         momentum: false,
         snap: 'li'
     });
-    document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+    // document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
 });
+
+function showSuccessDialog() {
+    $('.module-share-success').css({
+        width: '100%',
+        height: $(document).height()
+    }).show();
+
+    var timer = setTimeout(function () {
+        $('.module-share-success').hide();
+        $('.module-detail').removeClass('show');
+        $('.module-cover').show();
+    }, 1 * 1000);
+
+    $('.module-share-success .btn-close').unbind().on('tap', function () {
+        timer && clearInterval(timer);
+        $('.module-share-success').hide();
+    });
+    $('.module-share-success .btn-use').unbind().on('tap', function () {
+        timer && clearInterval(timer);
+        // 使用优惠券逻辑
+    });
+
+    
+}
