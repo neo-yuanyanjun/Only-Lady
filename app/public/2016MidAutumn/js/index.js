4ee0a5642ec7c9a879bb577bc6523b46c553af90
yuanyanjun <yuanyanjun@baidu.com>
2016-08-30 20:46:47 +0800

    3

--- app/public/2016MidAutumn/js/index.js
+++ app/public/2016MidAutumn/js/index.js
@@ -4,5 +4,55 @@
  */
 
 $(document).ready(function () {
+    /**
+     * 加载百分比
+     */
+    var $percent = $('.wrapper-percent');
+    var durantion = 0.5 * 1000;
+    var start = new Date().getTime();
+    var timer = setInterval(function () {
+        var now = new Date().getTime();
+        if (now - start > durantion) {
+            clearInterval(timer);
+            $('.module-loading').hide();
+            $('.module-guide').addClass('show');
+        }
 
+        var elapse = now - start;
+        var percent = Math.floor(elapse / durantion * 100);
+        percent = Math.min(percent, 100);
+        $percent.html(percent + '%');
+    }, 100);
+
+    /**
+     * 生成两个整数之间的随机整数
+     */
+    function rand(min, max) {
+        return min + Math.floor(Math.random() * (max - min));
+    }
+
+    /**
+     * 模板
+     */
+    $('body').append($('#myTemplate').html());
+    $('#myTemplate').remove();
+
+
+    $('.module-guide').on('click', function () {
+        console.log('click');
+        $(this).remove();
+        $('.module-prologue').addClass('show');
+    });
+
+
+    /**
+     * iscroll
+     */
+    var myScroll = new window.IScroll('#wrapper-scroller', {
+        scrollX: true,
+        scrollY: false,
+        momentum: false,
+        snap: 'li'
+    });
+    document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
 });
